9f470e88214ed760fdf95ca2c000b5168165ef73
Jakub Muransky <jakub.muransky@innovatrics.com>
2024-08-26 14:18:32 +0200

    SGcommit

--- gapslib.py
+++ gapslib.py
@@ -129,16 +129,34 @@ def run():
         adbase = samdb_loc.get_default_basedn()
     if not adbase:
         adbase = "/etc/samba/smb.conf"
-
+    #Samba Group
+    if config.has_section('samba') and config.has_option('samba', 'group'):
+        group = config.get('samba', 'group')
+    else:
+        group = samdb_loc.get_default_basedn()
 
     testpawd = GetPasswordCommand()
     testpawd.lp = lp
     passwordattr = config.get('common', 'attr_password')
     allmail = {}
 
-    # Search all users
-    for user in samdb_loc.search(base=adbase, expression="(&(objectClass=user)(mail=*))", attrs=["mail","sAMAccountName"]):
-        mail = str(user["mail"])
+# Define the LDAP filter to find users who are members of the specified group
+user_filter = f"(&(objectClass=user)(memberOf={group}))"
+
+# Perform the LDAP search
+for user in samdb_loc.search(base=adbase, expression=user_filter, attrs=["mail", "sAMAccountName"]):
+    # Check if the mail attribute exists
+    if "mail" in user:
+        mail = user["mail"][0]  # Extract the mail attribute
+    else:
+        print(f"Error: User {user['sAMAccountName'][0]} does not have an email address.")
+        continue  # Skip to the next user if mail is not present
+    
+    # Extract the sAMAccountName attribute
+    sAMAccountName = user.get("sAMAccountName", [""])[0]  # Get sAMAccountName, default to an empty string if not present
+
+    # Print the user details
+    print(f"User: {sAMAccountName}, Email: {mail}")
         password = testpawd.get_account_attributes(samdb_loc,None,adbase,filter="(sAMAccountName=%s)" % (str(user["sAMAccountName"])),scope=ldb.SCOPE_SUBTREE,attrs=[passwordattr],decrypt=False)
         if not passwordattr in password:
             continue
